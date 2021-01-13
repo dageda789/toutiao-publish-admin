acfb4247e890467e37ff4a37b932d667e4123a0d
大便搅拌器 <1940737178@qq.com>
2021-01-13 13:22:46 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
+
+// 项目的启动入口
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 加载全局样式文件
+import './style/index.less'
+
 Vue.config.productionTip = false
 
+// 创建vue 跟实例
+// 把router,配置到跟实例中
+// 通过render 方法把app 根组件渲染到 #app 入口节点
 new Vue({
   router,
   render: h => h(App)
+  // el: #app  等价于$mount('app')
 }).$mount('#app')
